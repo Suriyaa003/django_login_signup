1b0f7edb8bf49b777960d9d2e2aeb781c6d56c9c
SURIYAA D <suriyaa3904@gmail.com>
2024-12-29 15:09:29 +0530

    Task view completed(36)

--- tudo_list/base/urls.py
+++ tudo_list/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import TaskList, TaskDetail
 
 
 urlpatterns = [
-    path('', views.tasklist, name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'), 
 ]
--- tudo_list/base/views.py
+++ tudo_list/base/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
-def tasklist(request):
-    return HttpResponse('To Do List')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
